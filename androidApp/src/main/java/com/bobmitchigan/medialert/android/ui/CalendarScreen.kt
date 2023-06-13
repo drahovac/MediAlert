973f0048e3e49428ff451257c426c849f9d681df
Vaclav Drahorad <drahorad.vac@gmail.com>
2023-06-13 22:05:30 +0200

    Draw gridlines over content

--- androidApp/src/main/java/com/bobmitchigan/medialert/android/ui/CalendarScreen.kt
+++ androidApp/src/main/java/com/bobmitchigan/medialert/android/ui/CalendarScreen.kt
@@ -30,7 +30,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.draw.drawWithContent
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.drawscope.DrawScope
@@ -268,7 +268,8 @@ private fun CalendarCells(
         Modifier
             .verticalScroll(verticalScroll)
             .background(MaterialTheme.colors.surface)
-            .drawBehind {
+            .drawWithContent {
+                drawContent()
                 drawGridLines(color)
             }) {
         for (row in 0..48) {
